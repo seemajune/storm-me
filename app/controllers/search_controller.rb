57404cd3d4aa5bce240e05d3d21f86c90f4c1583
seemajune <seemashariat@gmail.com>
2014-11-30 13:13:51 -0500

    successfully rendered first image view of search results via instagram API

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,21 @@
+require 'open-uri'
+require 'json'
+
 class SearchController < ApplicationController
+
+  def index
+    base_url = 'https://api.instagram.com/v1/tags/'
+    query_string = '/media/recent?access_token=50668157.5b9e1e6.0347384d4d26410089b8236038578950'
+    json = open(base_url + 'weather' + query_string).read 
+    json_data = JSON.parse(json)
+
+    all_image_results = json_data["data"]
+
+    all_image_links = []
+      all_image_results.each do |result, value|
+        all_image_links.push(result["images"]["thumbnail"]["url"])
+      end
+      @all_image_links = all_image_links
+  end
+
 end
