38769cf4f80ce4ef071caad1206c03255ea755ea
seemajune <seemashariat@gmail.com>
2015-04-05 09:25:46 -0400

    feeds russian circles related images

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,43 +5,23 @@ class SearchController < ApplicationController
 
   def index
     base_url = 'https://api.instagram.com/v1/tags/'
-    query_string = 'weather/media/recent?access_token='
+    query_string = 'russiancircles/media/recent?access_token='
     token = '50668157.5b9e1e6.0347384d4d26410089b8236038578950'
 
     json = open(base_url + query_string + token).read 
     json_data = JSON.parse(json)
-
     all_image_results = json_data["data"]
-
-    all_image_thumbnails = []
-    locations = [];
+    all_images = Hash.new
     all_image_results.each do |result, value|
-      #all_image_thumbnails.push(result["images"]["thumbnail"]["url"])
       if result["location"]
-        locations.push(result["images"]["thumbnail"]["url"], result["location"])
+        all_images[result["images"]["thumbnail"]["url"]] = result["location"]
       end
-      locations
+      all_images
     end
-    @all_image_thumbnails = all_image_thumbnails
-    @locations = locations
+    @all_images = all_images
   end
 
   def show
-    base_url = 'https://api.instagram.com/v1/tags/'
-    query_string = 'weather/media/recent?access_token='
-    token = '50668157.5b9e1e6.0347384d4d26410089b8236038578950'
-
-    json = open(base_url + query_string + token).read 
-    json_data = JSON.parse(json)
-
-    all_image_results = json_data["data"]
-
-    all_image_thumbnails = []
-
-    all_image_results.each do |result, value|
-      all_image_thumbnails.push(result["images"]["thumbnail"]["url"])
-    end
-    @all_image_thumbnails = all_image_thumbnails
   end
 
 end
